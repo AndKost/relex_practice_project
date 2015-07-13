9c9478b5bbf000a6a42e3c3f7e2fca4df78d0c45
Andrey Kostyuchenko <and-kost@mail.ru>
2015-07-13 11:45:28 +0300

    add comments value class

--- practice_project/DAL/src/main/java/org/DAL/Comment.java
+++ practice_project/DAL/src/main/java/org/DAL/Comment.java
@@ -1,6 +1,57 @@
 package org.DAL;
 
+import javax.persistence.*;
+
+import java.util.Date;
+
+@Entity
+@Table(name = "comments")
 public class Comment {
+	
+	public Comment() {
+		
+	}
+	
 	private int id;
-	, text, Date, numberOfVotes, author, interview
+	private String text;
+	private Date date;
+	private int numberOfVotes;
+	private User author;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getText() {
+		return text;
+	}
+	public void setText(String text) {
+		this.text = text;
+	}
+	public Date getDate() {
+		return date;
+	}
+	public void setDate(Date date) {
+		this.date = date;
+	}
+	public int getNumberOfVotes() {
+		return numberOfVotes;
+	}
+	public void setNumberOfVotes(int numberOfVotes) {
+		this.numberOfVotes = numberOfVotes;
+	}
+	public User getAuthor() {
+		return author;
+	}
+	public void setAuthor(User author) {
+		this.author = author;
+	}
+	public Interview getInterview() {
+		return interview;
+	}
+	public void setInterview(Interview interview) {
+		this.interview = interview;
+	}
+	private Interview interview;
 }
